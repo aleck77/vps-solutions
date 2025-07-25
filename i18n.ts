4d9af1c964f872b10a6287be7e7a5920b1148dd4
Oleksandr Oshatiuk <aleck541977@gmail.com>
2025-07-25 20:18:25 +0000

    не помогло или не 2 проблемы или за деревьями кто то леса на видит анали

--- i18n.ts
+++
@@ -1,6 +0,0 @@
-import {getRequestConfig} from 'next-intl/server';
- 
-export default getRequestConfig(async ({locale}) => ({
-  // Corrected the path to be relative from the project root.
-  messages: (await import(`src/messages/${locale}.json`)).default
-}));
